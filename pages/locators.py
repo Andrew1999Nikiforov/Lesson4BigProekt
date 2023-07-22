b10ee3e0534079adb2af49cf7218bcc0bb7634be
Andrew1999Nikiforov <niki.andrew1999@mail.ru>
2023-07-22 13:53:13 +0300

    Селекторы к формам регистрации и логина, добавьте их в класс LoginPageLocators

--- pages/locators.py
+++ pages/locators.py
@@ -3,3 +3,13 @@ from selenium.webdriver.common.by import By
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
     REGISTRATION_LINK = (By.CSS_SELECTOR, "#login_link")
+
+class LoginPageLocators():
+    ID_LOGIN_USERNAME = (By.ID, "id_login-username")
+    ID_LOGIN_PASSWORD = (By.ID, "id_login-password")
+    ID_LOGIN_BUTTON = (By.NAME, "login_submit")
+
+    ID_REGISTRATION_EMAIL = (By.ID, "id_registration-email")
+    ID_REGISTRATION_PASSWORD1 = (By.ID, "id_registration-password1")
+    ID_REGISTRATION_PASSWORD2 = (By.ID, "id_registration-password2")
+    ID_REGISTRATION_BUTTON = (By.NAME, "registration_submit")
