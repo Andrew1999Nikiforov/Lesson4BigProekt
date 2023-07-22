f864508371b8df624283332f49661398e934f0f8
Andrew1999Nikiforov <niki.andrew1999@mail.ru>
2023-07-22 13:54:25 +0300

    Удаление пробелов

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,7 +1,7 @@
 from .base_page import BasePage
 from selenium.webdriver.common.by import By
 from .locators import MainPageLocators
-
+import time
 class MainPage(BasePage):
     def go_to_login_page(self):
         login_link = self.browser.find_element(*MainPageLocators.REGISTRATION_LINK)
