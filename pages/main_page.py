5befc344566ac18f412b94173b3c11d96cdf11bb
Andrew1999Nikiforov <niki.andrew1999@mail.ru>
2023-07-22 22:36:52 +0300

    Удалил лишнюю функцию нажатия на кнопку добавить в корзину

--- pages/main_page.py
+++ pages/main_page.py
@@ -4,7 +4,3 @@ from .locators import MainPageLocators
 class MainPage(BasePage):
     def go_to_login_page(self):
         self.browser.find_element(*MainPageLocators.REGISTRATION_LINK).click()
-
-
-    def click_to_button_add_busket(self):
-        self.browser.find_element(*MainPageLocators.BUTTON_ADD_BUSKET).click()
