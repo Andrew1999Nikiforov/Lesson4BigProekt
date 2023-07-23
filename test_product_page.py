e4b502c791a9d75d8576a4637830e01b2cc8a3dd
Andrew1999Nikiforov <niki.andrew1999@mail.ru>
2023-07-23 20:47:34 +0300

    Вызов функций проверки соответствия цен и названий продуктов

--- test_product_page.py
+++ test_product_page.py
@@ -1,8 +1,12 @@
 from .pages.product_page import ProductPage
-
+import time
 def test_guest_can_add_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
     page = ProductPage(browser, link)
     page.open()
+    page.check_before_add_name_product()
     page.click_to_button_add_busket()
     page.solve_quiz_and_get_code()
+    page.check_after_add_name_product()
+    page.check_price_before_and_after_add_basket()
+    time.sleep(10)
