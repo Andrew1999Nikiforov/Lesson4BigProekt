cc1632942a5ec4b2db7a9067d1c7366aee5282f8
Andrew1999Nikiforov <niki.andrew1999@mail.ru>
2023-07-24 15:24:48 +0300

    Добавление новых функций и тестов по проверке успешного добавления товара в корзину

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,9 +1,12 @@
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support import expected_conditions as EC
 import math
 class BasePage():
 
-    def __init__(self, browser, url, timeout=10):
+    def __init__(self, browser, url, timeout=0):
         self.browser = browser
         self.url = url
         self.browser.implicitly_wait(timeout)
@@ -11,10 +14,25 @@ class BasePage():
     def open(self):
         self.browser.get(self.url)
 
-    def is_element_present(self, how, what):
+    def is_element_present(self, how, what): # Проверка, что на сайте имеется какой то элемент
         try:
             self.browser.find_element(how, what)
         except NoSuchElementException:
+            return False
+        return True
+    
+    def is_not_element_present(self, how, what, timeout=4): # Проверка, что на сайте не имеется какой то элемент
+        try:
+            WebDriverWait(self.browser, timeout).until(EC.presence_of_element_located((how, what)))
+        except TimeoutException:
+            return True
+        return False
+    
+    def is_disappeared(self, how, what, timeout=4):
+        try:
+            WebDriverWait(self.browser, timeout, 1, TimeoutException).\
+                until_not(EC.presence_of_element_located((how, what)))
+        except TimeoutException:
             return False
         return True
     
@@ -31,3 +49,5 @@ class BasePage():
             alert.accept()
         except NoAlertPresentException:
             print("No second alert presented")
+
+    
--- test_product_page.py
+++ test_product_page.py
@@ -1,7 +1,7 @@
 from .pages.product_page import ProductPage
 import time
 import pytest
-
+"""
 @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
@@ -21,3 +21,29 @@ def test_guest_can_add_product_to_basket(browser, link):
     page.solve_quiz_and_get_code()
     page.check_after_add_name_product()
     page.check_price_before_and_after_add_basket()
+"""
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.click_to_button_add_busket()
+    page.check_func_is_not_element_present_success_message_after_adding_product_to_basket()
+    
+def test_guest_cant_see_success_message(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.check_func_is_not_element_present_success_message_after_adding_product_to_basket()
+
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.click_to_button_add_busket()
+    page.check_func_is_disappeared_success_message_after_adding_product_to_basket()
+
+def test_message_disappeared(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.check_func_is_disappeared_success_message_after_adding_product_to_basket()
