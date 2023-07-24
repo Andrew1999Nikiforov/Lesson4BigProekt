4bd61a596240442b9a1831ef91498e01006c36ae
Andrew1999Nikiforov <niki.andrew1999@mail.ru>
2023-07-24 16:51:33 +0300

    Добавили функции для проверки страницы

--- test_product_page.py
+++ test_product_page.py
@@ -21,7 +21,7 @@ def test_guest_can_add_product_to_basket(browser, link):
     page.solve_quiz_and_get_code()
     page.check_after_add_name_product()
     page.check_price_before_and_after_add_basket()
-"""
+
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     page = ProductPage(browser, link)
@@ -47,3 +47,16 @@ def test_message_disappeared(browser):
     page = ProductPage(browser, link)
     page.open()
     page.check_func_is_disappeared_success_message_after_adding_product_to_basket()
+"""
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    page.should_be_login_link()
